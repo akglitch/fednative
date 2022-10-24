653e56a73e30e495a27e25911e91d7edd57a76f8
kaytee <mcaninakwa@gmail.com>
2022-10-24 06:25:38 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,41 +1,18 @@
-import React from "react";
-import { ImageBackground, Button, StyleSheet, Text, View } from "react-native";
 
-const image = require('./components/images/eng.png')
+import * as React from 'react';
+import Gallery from "./components/Gallery";
+import { View } from "react-native";
 
-const App = () => (
- 
-  <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Mr money with the vibes</Text>
-    </ImageBackground>
-    <Button
-          title="lets go"
-        
-        />
-  </View>
-);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
 
-  
-  },
-  text: {
-    color: "#000000",
-    fontSize: 32,
-    lineHeight: 104,
-    textAlign: "center",
-    
-    
- 
+
+
+const App = () => {
+  return(
+   <View>
+    <Gallery />
+   
+   </View>
+  )
   }
-  
-});
-
 export default App;
